04ee97263f409dd6cf54561c885e437729897069
ricklyon <ricklyon@github.com>
2025-08-17 08:48:17 -0600

    remove line number

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,6 @@ sphinx_gallery_conf = {
     'ignore_pattern': r'interactive_.*\.py',
     'example_extensions': {'.py'},
     'download_all_examples': False,
-    'line_numbers': True
 }
 
 # Make autosummary generate stub pages
