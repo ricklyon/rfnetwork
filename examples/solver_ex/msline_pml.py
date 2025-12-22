19403ceefa3c6f080b46e78a2d195d6fafb65834
ricklyon <ricklyon@github.com>
2025-12-22 16:52:16 -0700

    add s21 measurements

--- examples/solver_ex/msline_pml.py
+++ examples/solver_ex/msline_pml.py
@@ -18,15 +18,15 @@ u0 = const.u0
 e0 = const.e0
 c0 = const.c0
 
-ms_w = 0.040
+ms_w = 0.04
 ms1_y = 0
 
-sbox_h = 0.3
+sbox_h = 0.25
 sbox_w = 0.4
 sbox_len = 1
 
 sub_h = 0.02
-ms_x = (-sbox_len/2 + 0.1, sbox_len/2 - 0.1)
+ms_x = (-sbox_len/2 + 0.1, sbox_len/2)
 
 line = rfn.elements.MSLine(h=sub_h, er=3.66, w=ms_w)
 z_ref = line.get_properties(10e9).sel(value="z0").item()
@@ -48,12 +48,6 @@ port1_face = pv.Rectangle([
     (ms_x[0], ms1_y + ms_w/2, 0),
 ])
 
-port2_face = pv.Rectangle([
-    (ms_x[1], ms1_y - ms_w/2, sub_h),
-    (ms_x[1], ms1_y + ms_w/2, sub_h),
-    (ms_x[1], ms1_y + ms_w/2, 0),
-])
-
 
 current_face = pv.Rectangle([
     (0, ms1_y - ms_w/2 - 0.001, sub_h + 0.001),
@@ -66,11 +60,10 @@ voltage_line = pv.Line(
     [0, ms1_y, 0], [0, ms1_y, sub_h]
 )
 
-s = rfn.Solver_PCB(sbox, nports=2)
+s = rfn.Solver_PCB(sbox, nports=1)
 s.add_substrate("sub", substrate, er=3.66, opacity=0.0)
 s.add_pec_face("ms1", ms1_trace, color="gold")
 s.add_lumped_port(1, port1_face)
-s.add_lumped_port(2, port2_face)
 
 self = s
 
@@ -85,7 +78,7 @@ s.init_mesh(d0 = 0.02, n0 = 3, d_pec = 0.01, n_min_pec=3, d_sub=0.005, n_min_sub
 s.init_coefficients()
 
 s.init_ports()
-# s.add_xPML(side="upper")
+s.add_xPML(side="upper")
 s.init_pec()
 
 s.add_field_monitor("mon1", "ez", "z", sub_h - 0.005, 10)
@@ -96,27 +89,27 @@ s.add_current_probe("c1", current_face)
 s.add_voltage_probe("v1", voltage_line)
 
 
-plotter = s.render(show_probes=True)
-plotter.camera_position = "yz"
-plotter.show()
+# plotter = s.render(show_probes=True)
+# plotter.camera_position = "yz"
+# plotter.show()
 
 
-Db_0 = s.dt / u0
-Cb_0 = s.dt / e0 
-p = s.plot_cooeficients("ez_x", "a", "y", 0, point_size=15, cmap="brg")
-p.camera_position = "xy"
-p.show()
+# Db_0 = s.dt / u0
+# Cb_0 = s.dt / e0 
+# # p = s.plot_cooeficients("ey_x", "b", "z", sub_h, point_size=15, cmap="brg", normalization=Cb_0)
+# # p.camera_position = "xy"
+# # p.show()
 
 f0 = 10e9
-pulse_n = 1600
+pulse_n = 1200
 # width of half pulse in time
-t_half = (s.dt * 100)
+t_half = (s.dt * 250)
 # center of the pulse in time
 t0 = (s.dt * 500)
 
 t = np.linspace(0, s.dt * pulse_n, pulse_n)
 vsrc = 1e-2 * (np.sin(2* np.pi * f0 * (t)) * np.exp(-((t - t0) / t_half)**2)).astype(np.float32)
-plt.plot(vsrc)
+# plt.plot(vsrc)
 
 frequency: np.ndarray = np.arange(5e9, 15e9, 10e6)
 
@@ -124,7 +117,6 @@ s.run([1], [vsrc], n_threads=4)
 
 sdata = s.get_sparameters(frequency)
 S11 = sdata[:, 0]
-S21 = sdata[:, 1]
 
 
 p = s.plot_monitor(["mon1"], el=0, zoom=1.1, az=0, view="xy", opacity=[0.8, 1], linear=False, cmap="jet", style="surface")
@@ -154,12 +146,6 @@ rfn.plots.draw_smithchart(ax)
 plt.plot(S11.real, S11.imag)
 plt.plot(S11_z.real, S11_z.imag)
 
-fig, ax = plt.subplots()
-ax.plot(frequency / 1e9, conv.db20_lin(S11))
-ax.plot(frequency / 1e9, conv.db20_lin(S21))
-mplm.line_marker(x = 10, axes=ax)
-ax.set_xlabel("Frequency [GHz]")
-ax.set_ylabel("[dB]")
-ax.legend(["S11", "S22"])
-
+# fig, ax = plt.subplots()
+# plt.plot(frequency, conv.db20_lin(S11))
 plt.show()
