c277824275e2e0e9630f3ef5391c67a45bf9ca97
ricklyon <ricklyon@github.com>
2026-01-05 20:44:21 -0700

    minor tweaks to example

--- examples/solver_ex/coupled_slines.py
+++ examples/solver_ex/coupled_slines.py
@@ -36,7 +36,7 @@ er = 1
 
 sbox_h = b
 sbox_w = 1.5
-sbox_len = 4
+sbox_len = 2
 
 
 ms_x = (-sbox_len/2 + 0.3, sbox_len/2)
@@ -44,15 +44,20 @@ ms_x = (-sbox_len/2 + 0.3, sbox_len/2)
 line_ref = rfn.elements.Stripline(w=s_w, b=b, er=er, length=sbox_len - 0.3)
 z_ref = line_ref.get_properties(10e9).sel(value="z0").item()
 
-# page 174 in Matthaei, even and odd mode impedances of coupled strip line
-k_e = np.tanh((np.pi / 2) * (s_w / b)) * np.tanh((np.pi / 2) * (s_w + s_sp) / b)
-kp_e = np.sqrt(1 - (k_e **2))
+Z0_o, Z0_e = utils.coupled_sline_impedance(s_w, s_sp, b, er)
+###
+vp = rfn.const.c0 / np.sqrt(er)
+# even mode capacitance
+c_e = 1 / (vp * Z0_e)
+print("even c/e", c_e / const.e0)
 
-k_o = np.tanh((np.pi / 2) * (s_w / b)) * (1 / np.tanh((np.pi / 2) * (s_w + s_sp) / b))
-kp_o = np.sqrt(1 - (k_o **2))
+# odd mode capacitance
+c_o = 1 / (vp * Z0_o)
+print("odd c/e", c_o / const.e0)
 
-Z0_e = ((30 * np.pi) / (np.sqrt(er))) * (ellipk(kp_e) / ellipk(k_e))
-Z0_o = ((30 * np.pi) / (np.sqrt(er))) * (ellipk(kp_o) / ellipk(k_o))
+# mutual capacitance
+c_m = ((c_o - c_e) / 2)
+print("c_m", c_m / const.e0)
 
 
 substrate = pv.Cube(center=(0, 0, sub_h), x_length=sbox_len, y_length=sbox_w, z_length=b)
@@ -96,16 +101,19 @@ port4_face = pv.Rectangle([
 ])
 
 current_face = pv.Rectangle([
-    (0, s1_y - s_w/2 - 0.001, sub_h + 0.001),
-    (0, s1_y + s_w/2 + 0.001, sub_h + 0.001),
-    (0, s1_y + s_w/2 + 0.001, sub_h - 0.001),
+    (-0.5, s1_y - s_w/2 - 0.001, sub_h + 0.001),
+    (-0.5, s1_y + s_w/2 + 0.001, sub_h + 0.001),
+    (-0.5, s1_y + s_w/2 + 0.001, sub_h - 0.001),
 ])
 
 
-voltage_line = pv.Line(
-    [0, s1_y, 0], [0, s1_y, sub_h]
+voltage_line1 = pv.Line(
+    [-0.5, s1_y, 0], [-0.5, s1_y, sub_h]
 )
 
+voltage_line2 = pv.Line(
+    [0.5, s1_y, 0], [0.5, s1_y, sub_h]
+)
 
 s = rfn.Solver_PCB(sbox, nports=4)
 s.add_substrate("sub", substrate, er=er, opacity=0.0)
@@ -119,24 +127,25 @@ s.add_lumped_port(4, port4_face)
 
 self = s
 
-f0 = 2e9
+f0 = 1.5e9
 lam0 = rfn.const.c0_in / (f0 * np.sqrt(er))
 
-s.init_mesh(d0 = lam0/20, n0 = 4, d_pec = lam0/30, n_min_pec=4, d_sub=lam0/20, n_min_sub=5, blend_pec=False)
+s.init_mesh(d0 = 0.3, n0 = 4, d_pec = 0.3, n_min_pec=4, d_sub=0.3, n_min_sub=5, blend_pec=False)
 s.init_coefficients()
 
 s.init_ports(r0=100)
 s.add_xPML(side="upper")
-s.init_pec(edge_correction=True)
+s.init_pec(edge_correction=False)
 
 s.add_field_monitor("mon1", "ez", "z", sub_h - 0.005, 10)
 
 s.add_current_probe("c1", current_face)
-s.add_voltage_probe("v1", voltage_line)
+s.add_voltage_probe("v1", voltage_line1)
+s.add_voltage_probe("v2", voltage_line2)
 
-# plotter = s.render(show_probes=True)
-# plotter.camera_position = "yz"
-# plotter.show()
+plotter = s.render(show_probes=True)
+plotter.camera_position = "yz"
+plotter.show()
 
 pulse_n = 1200
 # width of half pulse in time
@@ -149,7 +158,7 @@ vsrc = 1e-2 * (np.sin(2* np.pi * f0 * (t)) * np.exp(-((t - t0) / t_half)**2)).as
 # plt.plot(vsrc)
 
 
-frequency: np.ndarray = np.arange(1e9, 5e9, 10e6)
+frequency: np.ndarray = np.arange(0.5e9, 3e9, 10e6)
 
 # run even mode
 s.run([1, 2, 3, 4], [vsrc, -vsrc, vsrc, -vsrc], n_threads=4)
@@ -159,10 +168,11 @@ S11_even = sdata[:,1]
 
 # compute line impedance
 line_i = self.vi_probe_values("c1")
-line_v = self.vi_probe_values("v1")
+line_v1 = self.vi_probe_values("v1")
+line_v2 = self.vi_probe_values("v2")
 
-plt.plot(line_v)
-plt.plot(vsrc)
+plt.plot(line_v1)
+plt.plot(line_v2)
 
 IP = utils.dtft(self.vi_probe_values("c1"), frequency, 1 / s.dt)
 VP = utils.dtft(self.vi_probe_values("v1"), frequency, 1 / s.dt) * np.exp(1j * 2 * np.pi * frequency * (-s.dt / 2))
@@ -182,15 +192,11 @@ def get_vp(v1, v2, d):
 
     return np.abs(d) / delta_t
 
-vp_e = get_vp(vsrc, line_v, conv.m_in(ms_x[0]))
+vp_e = get_vp(line_v1, line_v2, conv.m_in(1))
 
-print("even vp", vp_e / rfn.const.c0)
-# even mode capacitance
-c_e = 1 / (vp_e * Z0_e)
-print("even c/e", c_e / const.e0)
 
-# p = s.plot_monitor(["mon1"], el=0, zoom=1.1, az=0, view="xy", opacity=[0.8, 1], linear=False, cmap="jet", style="surface")
-# p.show(title="EM Solver")
+p = s.plot_monitor(["mon1"], el=0, zoom=1.1, az=0, view="xy", opacity=[0.8, 1], linear=False, cmap="jet", style="surface")
+p.show(title="EM Solver")
 
 # run odd mode
 s.run([1, 2, 3, 4], [vsrc, -vsrc, -vsrc, vsrc], n_threads=4)
@@ -201,24 +207,15 @@ S11_odd = sdata[:, 1]
 
 # compute line impedance
 line_i = self.vi_probe_values("c1")
-line_v = self.vi_probe_values("v1")
+line_v1 = self.vi_probe_values("v1")
+line_v2 = self.vi_probe_values("v2")
 
 IP = utils.dtft(self.vi_probe_values("c1"), frequency, 1 / s.dt)
 VP = utils.dtft(self.vi_probe_values("v1"), frequency, 1 / s.dt) * np.exp(1j * 2 * np.pi * frequency * (-s.dt / 2))
 ZP_odd = VP / IP
 
-vp_o = get_vp(vsrc, line_v,  conv.m_in(ms_x[0]))
-print("odd vp", vp_o / rfn.const.c0)
 
-# odd mode capacitance
-c_o = 1 / (vp_o * Z0_e)
-print("odd c/e", c_o / const.e0)
-
-# mutual capacitance
-c_m = (c_o - c_e / 2)
-print("c_m", c_m / const.e0)
-
-# p = s.plot_monitor(["mon1"], el=0, zoom=1.1, az=0, view="xy", opacity=[0.8, 1], linear=True, cmap="jet", style="surface")
+# p = s.plot_monitor(["mon1"], el=0, zoom=1.1, az=0, view="xy", opacity=[0.8, 1], linear=False, cmap="jet", style="surface")
 # p.show(title="EM Solver")
 
 
@@ -226,8 +223,8 @@ print("c_m", c_m / const.e0)
 
 fig, ax = plt.subplots()
 # plt.plot(frequency / 1e9, ZP.real)
-ax.plot(frequency / 1e9, np.real(conv.z_gamma(S11_even)))
-ax.plot(frequency / 1e9, np.real(conv.z_gamma(S11_odd)))
+# ax.plot(frequency / 1e9, np.real(conv.z_gamma(S11_even)))
+# ax.plot(frequency / 1e9, np.real(conv.z_gamma(S11_odd)))
 ax.plot(frequency / 1e9, np.real(ZP_even))
 ax.plot(frequency / 1e9, np.real(ZP_odd))
 plt.ylim([0, 200])
@@ -237,14 +234,12 @@ plt.axhline(y=Z0_o, linestyle=":", color="orange")
 ax.set_xlabel("Frequency [GHz]")
 ax.set_ylabel("Impedance [Ohm]")
 ax.legend(["Even Mode", "Odd Mode"])
-mplm.line_marker(x = 0, axes=ax)
+mplm.line_marker(x = f0/1e9, axes=ax)
 
-# S11_z = conv.gamma_z(ZP)
-
-fig, ax = plt.subplots()
-rfn.plots.draw_smithchart(ax)
-plt.plot(S11_even.real, S11_even.imag)
-plt.plot(S11_odd.real, S11_odd.imag)
+# fig, ax = plt.subplots()
+# rfn.plots.draw_smithchart(ax)
+# plt.plot(S11_even.real, S11_even.imag)
+# plt.plot(S11_odd.real, S11_odd.imag)
 
 # fig, ax = plt.subplots()
 # plt.plot(frequency, conv.db20_lin(S11))
