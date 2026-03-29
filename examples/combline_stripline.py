c2d097293a5a557d447def10714d52b69672748f
ricklyon <ricklyon@github.com>
2026-03-28 22:22:04 -0600

    use new util function for fringing cap

--- examples/combline_stripline.py
+++ examples/combline_stripline.py
@@ -1,22 +1,20 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-from rfnetwork import const, conv, utils
-import pyvista as pv
-import time
-from scipy.special import ellipk
-from matplotlib.ticker import ScalarFormatter
-from pathlib import Path
 
-import rfnetwork as rfn
-import mpl_markers as mplm
+from pathlib import Path
 import sys
 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pyvista as pv
 from scipy import optimize
+
+import rfnetwork as rfn
+
+
 
 # pv.set_jupyter_backend("trame")
 np.set_printoptions(suppress=True)
 
-plt.style.use('ggplot')
+# plt.style.use('ggplot')
 # Set the font family to serif, with "Times New Roman" as the preferred serif font
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["font.serif"] = ["Times New Roman"] + plt.rcParams["font.serif"]
@@ -25,10 +23,9 @@ sys.argv = sys.argv[0:1]
 
 dir_ = Path(__file__).parent
 
-u0 = const.u0
-e0 = const.e0
-c0 = const.c0
-eta0 = const.eta0
+e0 = rfn.const.e0
+c0 = rfn.const.c0
+eta0 = rfn.const.eta0
 
 er = 3.66
 
@@ -42,8 +39,28 @@ Ck, Cmk = rfn.utils.combline_sections_nb(g, f1, f2, er=er, h=0.25)
 
 Cmk = (Cmk * 0.85)
 
-def Cab_error(sp, target_Cab, w):
-   Cf_o, Cf_e = utils.coupled_sline_fringing_cap(w, sp, b, er)
+# from https://ieeexplore.ieee.org/document/1124973
+Cfp = 0.4407
+
+t = 0.001
+w = 0.02
+
+s = 0.01
+er = 3.66
+
+tb = t / b
+sb = s / b
+
+# from https://ieeexplore.ieee.org/document/1124973
+Cfe = (2 / np.pi) * np.log( 1 + np.tanh(np.pi * s / (2 * b)))
+Cfo = (2 / np.pi) * np.log( 1 + (1 / np.tanh(np.pi * s / (2 * b))))
+
+rfn.utils.coupled_sline_fringing_cap(s, b, er)
+
+
+
+def Cab_error(sp, target_Cab):
+   Cf_o, Cf_e = rfn.utils.coupled_sline_fringing_cap(sp, b, er)
    # normalized Cab
    Cab = (Cf_o - Cf_e) / (e0 * er)
    # return error between realized interline capacitance and target value
@@ -55,16 +72,15 @@ wk = np.ones_like(Ck) * (b / 2)
 
 for m in range(5):
     for i, cmk in enumerate(Cmk):
-        w = wk[i+1] if i < (len(wk) - 1) else wk[i-1]
-        sk[i] = optimize.least_squares(Cab_error, x0=b*0.5, args=(cmk, w), bounds=(0.001, b)).x[0]
+        sk[i] = optimize.least_squares(Cab_error, x0=b*0.5, args=(cmk,), bounds=(0.001, b)).x[0]
 
     # even mode fringing capacitance for each space between lines, width is arbitrary here
     Cf_e = np.array([
-        utils.coupled_sline_fringing_cap(wk[i+1] if i < len(wk) - 2 else wk[i-1], s, b, er)[1] for i, s in enumerate(sk)
+        rfn.utils.coupled_sline_fringing_cap(s, b, er)[1] for i, s in enumerate(sk)
     ]) / (e0 * er)
 
     # fringing capacitance on the outer edges (not between the two lines), figure 5.05-10b, for t=0
-    Cf = 0.44
+    Cf = 0.4407
     # Ck is the even mode capacitance Ce. Use equation 5.05-25 to determine the per unit length parallel plate 
     # capacitance for each line. Normalized by eps
     Cf_eps_left = np.concatenate([[Cf], Cf_e])
@@ -83,32 +99,12 @@ ymax = rfn.const.c0_in / (np.sqrt(f1 * f2) * np.sqrt(er) * 4)
 
 y0 = 0.095
 y1 = ymax - y0
-via_size = 0.03
+via_size = 0.02
 
-w0 = 0.035
+w50 = 0.035
 feed_y = 0.31
 feed_len = 0.12
 
-# sk = np.array(sk) + 0.003
-# wk = np.array(wk) + 0.003
-
-# sk[1] += 0.002
-# sk[4] += 0.002
-
-# sk[0] += 0.001
-# sk[2] += 0.003
-# sk[3] += 0.003
-# sk[3] += 0.001
-
-# values that work
-ref_wk = np.array([0.02266854, 0.02478139, 0.02500937, 0.02477977, 0.02266854])
-ref_sk = np.array([0.01674886, 0.01947338, 0.01947338, 0.01674886])
-
-# [0.01966854 0.02178139 0.02200937 0.02177977 0.01966854]
-# [0.01574886 0.01947338 0.01947338 0.01574886]
-
-# print("wk", wk - ref_wk)
-print("sk", sk - ref_sk)
 
 print(sk)
 print(wk)
@@ -118,8 +114,6 @@ x0_k = np.zeros(K)
 x1_k = np.zeros(K)
 y0_k = np.zeros(K)
 y1_k = np.zeros(K)
-
-
 
 for i in range(K):
     if i % 2: # if odd
@@ -138,7 +132,7 @@ for i in range(1, K):
 
 y1_k[0] += 0.065
 y0_k[1] += 0.01
-# y1_k[2] -= 0.02
+y1_k[2] -= 0.005
 y0_k[3] += 0.01
 y1_k[4] += 0.065
 
@@ -175,15 +169,15 @@ for i in range(K):
     s.add_conductor(via, style=dict(color="gold", opacity=0.6))
 
 feed_1 = pv.Rectangle([
-        (x0_k[0] - feed_len, feed_y-w0/2, 0),
-        (x0_k[0] - feed_len, feed_y+w0/2, 0),
-        (x0_k[0], feed_y+w0/2, 0),
+        (x0_k[0] - feed_len, feed_y-w50/2, 0),
+        (x0_k[0] - feed_len, feed_y+w50/2, 0),
+        (x0_k[0], feed_y+w50/2, 0),
 ])
 
 feed_2 = pv.Rectangle([
-        (x1_k[-1] + feed_len, feed_y-w0/2, 0),
-        (x1_k[-1] + feed_len, feed_y+w0/2, 0),
-        (x1_k[-1], feed_y+w0/2, 0),
+        (x1_k[-1] + feed_len, feed_y-w50/2, 0),
+        (x1_k[-1] + feed_len, feed_y+w50/2, 0),
+        (x1_k[-1], feed_y+w50/2, 0),
 ])
 
 s.add_conductor(feed_1, style=dict(color="gold"))
@@ -191,15 +185,15 @@ s.add_conductor(feed_2, style=dict(color="gold"))
 
 
 port1_face = pv.Rectangle([
-    (x0_k[0] - feed_len, feed_y-w0/2, -sbox_h/2),
-    (x0_k[0] - feed_len, feed_y+w0/2, -sbox_h/2),
-    (x0_k[0] - feed_len, feed_y+w0/2, sbox_h/2),
+    (x0_k[0] - feed_len, feed_y-w50/2, -sbox_h/2),
+    (x0_k[0] - feed_len, feed_y+w50/2, -sbox_h/2),
+    (x0_k[0] - feed_len, feed_y+w50/2, sbox_h/2),
 ])
 
 port2_face = pv.Rectangle([
-    (x1_k[-1] + feed_len, feed_y-w0/2, -sbox_h/2),
-    (x1_k[-1] + feed_len, feed_y+w0/2, -sbox_h/2),
-    (x1_k[-1] + feed_len, feed_y+w0/2, sbox_h/2),
+    (x1_k[-1] + feed_len, feed_y-w50/2, -sbox_h/2),
+    (x1_k[-1] + feed_len, feed_y+w50/2, -sbox_h/2),
+    (x1_k[-1] + feed_len, feed_y+w50/2, sbox_h/2),
 ])
 
 integration_line1 = pv.Line((x0_k[0] - feed_len, feed_y, -sbox_h/2), (x0_k[0] - feed_len, feed_y, 0))
@@ -213,16 +207,18 @@ plotter.show()
 
 s.generate_mesh(d0 = 0.02, d_edge = 0.005)
 
+s.pos_to_idx((x0_k[0], y0_k[0], 0))
+
 for i in range(K):
     if i == 0:
         s.edge_correction(
             (x0_k[i], y0_k[i], 0), 
-            (x0_k[i], feed_y-w0/2, 0), 
+            (x0_k[i], feed_y-w50/2, 0), 
             integration_line="x-"
         )
         s.edge_correction(
             (x0_k[i], y1_k[i], 0), 
-            (x0_k[i], feed_y+w0/2, 0), 
+            (x0_k[i], feed_y+w50/2, 0), 
             integration_line="x-"
         )
     else:
@@ -235,11 +231,11 @@ for i in range(K):
     if i == (K - 1):
         s.edge_correction(
             (x1_k[i], y0_k[i], 0), 
-            (x1_k[i], feed_y-w0/2, 0), 
+            (x1_k[i], feed_y-w50/2, 0), 
             integration_line="x+"
         )
         s.edge_correction(
-            (x1_k[i], feed_y+w0/2, 0), 
+            (x1_k[i], feed_y+w50/2, 0), 
             (x1_k[i], y1_k[i], 0), 
             integration_line="x+"
         )
@@ -257,7 +253,7 @@ for i in range(K):
 # print(s.Nx * s.Ny * s.Nz / 1e3, "kcells")
 
 
-# p = s.plot_coefficients("ey_z", "a", "z", 0, point_size=15, cmap="brg")
+# p = s.plot_coefficients("hz_x1", "b", "z", 0, point_size=15, cmap="brg")
 # p.camera_position = "xy"
 # p.show()
 
@@ -273,7 +269,7 @@ pulse_n = 50000
 # t0 = (s.dt * 400)
 
 # vsrc = 1e-2 * s.gaussian_source(s.dt * 300, t0= s.dt * 200, t_len = s.dt * pulse_n)
-vsrc = 1e-2 * s.gaussian_source(width=s.dt * 1000, t0=s.dt * 1000, t_len = pulse_n * s.dt)
+vsrc = 1e-2 * s.gaussian_source(width=s.dt * 1000, t0=s.dt * 600, t_len = pulse_n * s.dt)
 
 # t = np.linspace(0, s.dt * pulse_n, pulse_n)
 # vsrc = 1e-2 * (np.sin(2* np.pi * f0 * (t)) * np.exp(-((t - t0) / t_half)**2)).astype(np.float32)
@@ -302,8 +298,8 @@ fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 9), height_ratios=[1, 2], const
 rfn.plots.draw_smithchart(ax2)
 ax2.plot(S11.real, S11.imag)
 
-ax1.plot(frequency / 1e9, conv.db20_lin(S11))
-ax1.plot(frequency / 1e9, conv.db20_lin(S21))
+ax1.plot(frequency / 1e9, rfn.conv.db20_lin(S11))
+ax1.plot(frequency / 1e9, rfn.conv.db20_lin(S21))
 ax1.set_xlabel("Frequency [GHz]")
 ax1.set_xticks(np.arange(0.6, 2.6, 0.2))
 ax1.set_xlim([0.6, 2.4])
