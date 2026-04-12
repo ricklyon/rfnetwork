83be41f09153073f2ebaf698f6b92d9f20aaaf66
ricklyon <ricklyon@github.com>
2026-04-11 18:09:47 -0600

    updated to cp patch

--- examples/dp_patch.py
+++ examples/dp_patch.py
@@ -28,6 +28,15 @@ plt.style.use(rfn.DEFAULT_STYLE)
 pv.set_jupyter_backend("trame")
 sys.argv = sys.argv[0:1]
 
+# todo:
+# 1. make patch cp
+# 2. implement rhcp, lhcp, xyz polarizations
+# 3. add angled msline tests
+# 4. parallel threads for far-field calculation
+# 5. ldarray interpolation
+# 6. make function to combine s-data into a single matrix
+
+
 try:
     dir_ = Path(__file__).parent
 except:
@@ -41,20 +50,29 @@ f0 = 2.4e9
 lam0 = rfn.const.c0_in / f0
 
 # bottom substrate er
-er_btm = 3.5
-h_btm = conv.in_mm(1.6)
+er_btm = 3.66
+h_btm = 0.04
 # top substrate er
-er_top = 3.5
-h_top = conv.in_mm(1.6)
+er_top = 3.66
+h_top = 0.06
 
 # parameters from table 3-1 in [1]
 len_patch = conv.in_mm(30)
 w_patch = conv.in_mm(30)
 w_slot = conv.in_mm(1.55)
-len_slot = conv.in_mm(6)
-len_leg = conv.in_mm(4)
-w_ms = conv.in_mm(4.42)
-len_stub = conv.in_mm(15)
+len_slot = conv.in_mm(7.5)
+len_leg = conv.in_mm(4.5)
+w_ms = 0.09
+len_stub1 = 0.15#0.6
+len_stub2 = 0.55
+
+# upper slot offset (h-polarized patch)
+x_pos_h = conv.in_mm(-9.1)
+# lower (v-polarized patch)
+y_pos_v = conv.in_mm(-9.1)
+
+rfn.elements.MSLine(0.04, 3.5, z0=50)
+
 
 
 # %%
@@ -62,13 +80,13 @@ len_stub = conv.in_mm(15)
 # ------------------------
 
 # solve box
-sbox = pv.Cube(center=(0, 0, 0), x_length=w_patch*1.7, y_length=w_patch*1.7, z_length=lam0 / 5)
+sbox = pv.Cube(center=(0, 0, 0), x_length=w_patch*1.8, y_length=w_patch*1.8, z_length=lam0 / 5)
 
 # create model and add elements
 s = rfn.FDTD_Solver(sbox)
 
 # top substrate
-sub_x0, sub_x1, sub_y0, sub_y1 = (-w_patch * 0.6, w_patch * 0.6, -len_patch * 0.6, len_patch * 0.6)
+sub_x0, sub_x1, sub_y0, sub_y1 = (-w_patch * 0.8, w_patch * 0.8, -len_patch * 0.8, len_patch * 0.8)
 sub_top = pv.Box(bounds=(sub_x0, sub_x1, sub_y0, sub_y1, 0, h_top))
 sub_btm = pv.Box(bounds=(sub_x0, sub_x1, sub_y0, sub_y1, -h_btm, 0))
 s.add_dielectric(sub_top, er=er_top, style=dict(opacity=0.2))
@@ -77,14 +95,19 @@ s.add_dielectric(sub_btm, er=er_btm, style=dict(opacity=0.2))
 # center conductor layer with slot
 gnd_plane = pv.Rectangle([(sub_x0, sub_y0, 0), (sub_x0, sub_y1, 0), (sub_x1, sub_y1, 0)])
 # cutout slots
-slot_center = (-w_slot/2, w_slot/2, -len_slot/2, len_slot/2, 0, 0)
-slot_lower = (-len_leg/2, len_leg/2, -len_slot/2 - w_slot/2, -len_slot/2 + w_slot/2, 0, 0)
-slot_upper = (-len_leg/2, len_leg/2, len_slot/2 - w_slot/2, len_slot/2 + w_slot/2, 0, 0)
+slot_h = (x_pos_h-w_slot/2, x_pos_h+  w_slot/2, -len_slot/2, len_slot/2, 0, 0)
+leg1_h = (x_pos_h-len_leg/2, x_pos_h+len_leg/2, -len_slot/2 - w_slot/2, -len_slot/2 + w_slot/2, 0, 0)
+leg2_h = (x_pos_h-len_leg/2, x_pos_h+len_leg/2, len_slot/2 - w_slot/2, len_slot/2 + w_slot/2, 0, 0)
 
-for cutout in (slot_center, slot_lower, slot_upper):
+# # cutout slot for lower (V)
+slot_v = (-len_slot/2, len_slot/2, y_pos_v - w_slot/2, y_pos_v + w_slot/2, 0, 0)
+leg1_v = (-len_slot/2 - w_slot/2, -len_slot/2 + w_slot/2, y_pos_v - len_leg/2, y_pos_v + len_leg/2, 0, 0)
+leg2_v = (len_slot/2 - w_slot/2, len_slot/2 + w_slot/2, y_pos_v - len_leg/2, y_pos_v + len_leg/2, 0, 0)
+
+for cutout in (slot_h, leg1_h, leg2_h, slot_v, leg1_v, leg2_v):
     gnd_plane = gnd_plane.clip_box(cutout).extract_surface(algorithm="dataset_surface")
 
-s.add_conductor(gnd_plane, style=dict(color="k"))
+s.add_conductor(gnd_plane, style=dict(color="k", opacity=0.8))
 
 
 # create patch
@@ -93,30 +116,51 @@ patch = pv.Rectangle(
 )
 s.add_conductor(patch, style=dict(opacity=0.4))
 
-# microstrip feed trace
+# microstrip feed trace for H pol slot
 port_x = sub_x0 + 0.05
-ms_trace = pv.Rectangle(
-    [(port_x, -w_ms/2, -h_btm), (port_x, w_ms/2, -h_btm), (len_stub, w_ms/2, -h_btm)]
+ms_trace_h = pv.Rectangle(
+    [(port_x, -w_ms/2, -h_btm), (port_x, w_ms/2, -h_btm), (x_pos_h+len_stub1, w_ms/2, -h_btm)]
 )
-s.add_conductor(ms_trace)
+ms_stub_h = pv.Rectangle(
+    [(x_pos_h+len_stub1 - w_ms/2, 0, -h_btm), 
+     (x_pos_h+len_stub1 + w_ms/2, 0, -h_btm), 
+     (x_pos_h+len_stub1 + w_ms/2, len_stub2, -h_btm)]
+)
+s.add_conductor(ms_trace_h, ms_stub_h)
+
+# microstrip feed for V pol slot
+port_y = sub_y0 + 0.05
+ms_trace_v = pv.Rectangle(
+    [(-w_ms/2, port_y, -h_btm), (w_ms/2, port_y, -h_btm), (w_ms/2, y_pos_v + len_stub1, -h_btm)]
+)
+ms_stub_v = pv.Rectangle(
+    [(0, y_pos_v + len_stub1 - w_ms/2, -h_btm), 
+     (0, y_pos_v + len_stub1 + w_ms/2, -h_btm), 
+     (len_stub2, y_pos_v + len_stub1 + w_ms/2, -h_btm)]
+)
+s.add_conductor(ms_trace_v, ms_stub_v)
+
 
 # add port
 port1_face = pv.Rectangle([(port_x, -w_ms/2, 0), (port_x, w_ms/2, 0), (port_x, w_ms/2, -h_btm)])
 s.add_lumped_port(1, port1_face, "z-")
 
+port2_face = pv.Rectangle([(-w_ms/2, port_y, 0), (w_ms/2, port_y, 0), (w_ms/2, port_y, -h_btm)])
+s.add_lumped_port(2, port2_face, "z-")
+
 # PML boundaries
 s.assign_PML_boundaries("x-", "x+", "y-", "y+", "z+", "z-", n_pml=5)
 
-s.generate_mesh(d0 = 0.05, d_edge=0.02)
+s.generate_mesh(d0 = 0.08, d_edge=0.02)
 s.render().show()
 
 
-s.plot_coefficients("ex_z", "b", "z", 0).show()
+# s.plot_coefficients("ex_z", "b", "z", 0).show()
 
 # setup far-field monitor
 s.add_farfield_monitor(frequency=f0)
 
-s.add_field_monitor("mon1", "ez", "z", h_top, n_step=10)
+s.add_field_monitor("mon1", "ez", "z", 0, n_step=50)
 
 # # show model rendering
 # cpos = pv.CameraPosition(
@@ -129,20 +173,55 @@ s.add_field_monitor("mon1", "ez", "z", h_top, n_step=10)
 # plotter = s.render(show_mesh=False, show_rulers=False, axes=ax, camera_position=cpos)
 
 # %%
+# Animated Gif
+# ------------------------
+# vsrc1 = s.gaussian_modulated_source(f0, width=5000e-12, t0=2500e-12, t_len=5000e-12)
+
+
+# # apply a phase delay to the vertically polarized port to get RHCP
+# wt0 = np.pi / 2
+# t_delay = wt0 / (2 * np.pi * f0)
+# # number of steps that fit in the delay (rounded, no interpolation)
+# n_delay = int(np.around(t_delay / s.dt))
+# vsrc2 = np.roll(vsrc1, n_delay)
+
+# plt.plot(vsrc1)
+# plt.plot(vsrc2)
+
+# s.assign_excitation(vsrc1, 1)
+# s.assign_excitation(vsrc2, 2)
+
+# s.solve(n_threads=4)
+
+# gif_setup = dict(file = dir_ / "dp_patch.gif", fps=20, step_ps=20, stop_ps=2000)
+# s.plot_monitor("mon1", opacity="linear", gif_setup=gif_setup)
+
+
+# %%
 # Setup Excitation and Solve
 # ------------------------
-vsrc = s.gaussian_source(width=120e-12, t0=60e-12, t_len=10000e-12)
-# plt.plot(vsrc)
+vsrc1 = s.gaussian_modulated_source(f0, width=100e-12, t0=60e-12, t_len=21000e-12)
 
-s.assign_excitation(vsrc, 1)
+
+# apply a phase delay to the vertically polarized port to get RHCP
+wt0 = np.pi / 2
+t_delay = wt0 / (2 * np.pi * f0)
+# number of steps that fit in the delay (rounded, no interpolation)
+n_delay = int(np.around(t_delay / s.dt))
+vsrc2 = np.roll(vsrc1, n_delay)
+
+plt.figure()
+plt.plot(vsrc1)
+plt.plot(vsrc2)
+
+s.assign_excitation(vsrc1, 1)
+s.assign_excitation(vsrc2, 2)
+
 s.solve(n_threads=4)
-
-s.plot_monitor("mon1", opacity=1).show()
-
 
 # plot far-field cut along theta at phi=0
 theta_cut = rfn.conv.db10_lin(
-    s.get_farfield_gain(theta=np.arange(-180, 181, 2), phi=0).sel(polarization="thetapol")
+    s.get_farfield_gain(theta=np.arange(-180, 181, 2), phi=0, polarization=["rhcp", "lhcp"])
 )
 
 fig1, ax = plt.subplots(subplot_kw=dict(projection="polar"))
@@ -161,7 +240,7 @@ labels = [f"{d}°" for d in [0, 45, 90, 135, 180, -135, -90, -45]]
 ax.set_xticklabels(labels)
 
 ax.set_xlabel(r"$\theta$ [deg], $\phi$=0°")
-ax.legend(["{:.3f}GHz".format(f/1e9) for f in theta_cut.coords["frequency"]])
+ax.legend(["RHCP", "LHCP"])
 mplm.line_marker(x=0)
 plt.show()
 
@@ -172,9 +251,7 @@ plt.show()
 frequency: np.ndarray = np.arange(1.5e9, 3e9, 2e6)
 sdata = s.get_sparameters(frequency, downsample=False)
 
-S11 = ldarray(sdata[:, 0][..., None, None], coords=dict(frequency=sdata.coords["frequency"], b=[1], a=[1]))
-
-ant = rfn.Component_Data(S11)
+ant = rfn.Component_Data(sdata)
 
 fig, ax = plt.subplots()
 ant.plot(11, fmt="smith", axes=ax)
@@ -185,5 +262,11 @@ ant.plot(11, fmt="db", axes=ax)
 
 
 mplm.line_marker(x=f0 / 1e9, xlabel=True)
-plt.show()
+
+
+fig, ax = plt.subplots()
+ant.plot(21, fmt="db", axes=ax)
 # %%
+plt.show()
+
+
